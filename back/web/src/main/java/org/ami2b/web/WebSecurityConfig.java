d5f90da01c92d9bb234e9be1531f68a04b721dd7
Étienne Morice <pro.emorice@mail.com>
2019-10-09 02:18:32 +0200

    Endpoint for authentication and auth, token yet to add

--- back/web/src/main/java/org/ami2b/web/WebSecurityConfig.java
+++ back/web/src/main/java/org/ami2b/web/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package org.ami2b.web;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,7 +11,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 				.antMatchers("/api/hello").permitAll()
-				.anyRequest().authenticated();
+				.antMatchers("/api/login").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.csrf().ignoringAntMatchers("/api/**");
 
 	}
 }
--- back/web/src/main/java/org/ami2b/web/auth/AuthController.java
+++ back/web/src/main/java/org/ami2b/web/auth/AuthController.java
@@ -1,9 +1,18 @@
 package org.ami2b.web.auth;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +20,24 @@ public class AuthController {
 	@GetMapping("/hello")
 	public String getHello() {
 		return "Hello";
+	}
+
+
+//@Resource(name="org.springframework.security.authenticationManager")
+//	private AuthenticationManager authenticationManager;
+	@Autowired
+        private AuthenticationManagerBuilder builder;
+
+	static class Credentials {
+		public String login;
+		public String password;
+	}
+
+	@PostMapping("/login")
+	public String login(@RequestBody Credentials creds) {
+		Authentication authIn = new UsernamePasswordAuthenticationToken(creds.login, creds.password);
+		builder.getObject().authenticate(authIn);
+		return creds.login;
 	}
 
 	@GetMapping("/vip_hello")
